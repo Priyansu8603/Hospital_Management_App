2038116eafbb080e475b2f1549e959816bcc86b4
Priyansu8603 <priyanshukumar238202@gmail.com>
2024-06-28 18:48:23 +0530

    Building UI and resources

--- app/src/main/java/com/example/hospital_management_app/UI/Activity/MainActivity.kt
+++ app/src/main/java/com/example/hospital_management_app/UI/Activity/MainActivity.kt
@@ -1,8 +1,19 @@
 package com.example.hospital_management_app.UI.Activity
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.hospital_management_app.R
 import com.example.hospital_management_app.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,6 +22,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Setup bottom navigation with nav controller
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+
+        // Handle window insets for FragmentContainerView
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.navHostFragment)) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(bottom = systemBars.bottom)
+            insets
+        }
+
+        // Show/hide bottom navigation based on the destination
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment, R.id.favouriteDoctor, R.id.myAppointment, R.id.paymentsFragment ->
+                    bottomNavigationView.visibility = View.VISIBLE
+                else -> bottomNavigationView.visibility = View.GONE
+            }
+        }
+
+        // Adjust window soft input mode to resize when keyboard is shown
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
 
     }
 }
